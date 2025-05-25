08df6fcb6df22566090602f2a5c4d1484e2e68a8
Mah <timohaarsentiev1@gmail.com>
2025-05-25 20:57:08 +0300

    Fixes

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public double calculate(double height, int weight) {
+    public int calculate(double height, int weight) {
         return (int) (weight / Math.pow(height,2)); // Changes
     }
 }
